eceb7187784b1e886569facca3c0de074c3d97a7
Lorenzo <lorenzo.santos3473@gmail.com>
2023-10-02 20:14:20 -0400

    Add drive to apriltag

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/driveToAprilTagMecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/driveToAprilTagMecanum.java
@@ -27,11 +27,16 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.robotcontroller.external.samples;
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import android.util.Size;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.VisionPortal;
@@ -48,10 +53,8 @@ import java.util.List;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
  */
 @TeleOp(name = "Concept: AprilTag", group = "Concept")
-//@Disabled
-public class ConceptAprilTag extends LinearOpMode {
-
-    private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
+// @Disabled
+public class driveToAprilTagMecanum extends LinearOpMode {
 
     /**
      * The variable to store our instance of the AprilTag processor.
@@ -62,9 +65,21 @@ public class ConceptAprilTag extends LinearOpMode {
      * The variable to store our instance of the vision portal.
      */
     private VisionPortal visionPortal;
+    private DcMotor FRM = null;
+    private DcMotor BRM = null;
+    private DcMotor FLM = null;
+    private DcMotor BLM = null;
+
+    static public double[] values = new double[4];
+    static public int id = 1;
 
     @Override
     public void runOpMode() {
+        //Map motors
+        FRM = hardwareMap.get(DcMotorEx.class, "frontRight");
+        BRM = hardwareMap.get(DcMotorEx.class, "backRight");
+        FLM = hardwareMap.get(DcMotorEx.class, "frontLeft");
+        BLM = hardwareMap.get(DcMotorEx.class, "backLeft");
 
         initAprilTag();
 
@@ -75,25 +90,43 @@ public class ConceptAprilTag extends LinearOpMode {
         waitForStart();
 
         if (opModeIsActive()) {
-            while (opModeIsActive()) {
-
-                telemetryAprilTag();
-
-                // Push telemetry to the Driver Station.
-                telemetry.update();
-
-                // Save CPU resources; can resume streaming when needed.
-                if (gamepad1.dpad_down) {
-                    visionPortal.stopStreaming();
-                } else if (gamepad1.dpad_up) {
-                    visionPortal.resumeStreaming();
+            updateValues();
+//            double x = values[0];
+//            double y = values[1];
+//            double yaw = values[2];
+//            double bearing = values[3];
+            //correct yaw to align robot parallel to backdrop
+            while (values[2]>=1 || values[2]<=-1)
+            {
+                //TO DO add acceleration turning
+                if(values[2]>0){
+                    FRM.setPower(0.1);
+                    BRM.setPower(0.1);
+                    FLM.setPower(-0.1);
+                    BLM.setPower(-0.1);
                 }
+                else if(values[2]<0){
+                    FLM.setPower(0.1);
+                    BLM.setPower(0.1);
+                    FRM.setPower(-0.1);
+                    BRM.setPower(-0.1);
+                }
+                updateValues();
+            }
+            while (values[0]!=0 && values[1]!=10)
+            {
+                //TO DO scale direction and power to difference to desired distance
+                double speed = (values[0]-0)/5 + (values[1]-10)/5;
+                //TO DO make sure angles line up with wheel direction
+                double direction = values[3] * (Math.PI / 180);
+                FLM.setPower(speed*(Math.sin(direction + Math.PI / 4.0)));
+                FRM.setPower(speed*(Math.cos(direction + Math.PI / 4.0)));
+                BLM.setPower(speed*(Math.cos(direction + Math.PI / 4.0)));
+                BRM.setPower(speed*(Math.sin(direction + Math.PI / 4.0)));
 
-                // Share the CPU.
-                sleep(20);
+                updateValues();
             }
         }
-
         // Save more CPU resources when camera is no longer needed.
         visionPortal.close();
 
@@ -106,37 +139,26 @@ public class ConceptAprilTag extends LinearOpMode {
 
         // Create the AprilTag processor.
         aprilTag = new AprilTagProcessor.Builder()
-            //.setDrawAxes(false)
-            //.setDrawCubeProjection(false)
-            //.setDrawTagOutline(true)
-            //.setTagFamily(AprilTagProcessor.TagFamily.TAG_36h11)
-            //.setTagLibrary(AprilTagGameDatabase.getCenterStageTagLibrary())
-            //.setOutputUnits(DistanceUnit.INCH, AngleUnit.DEGREES)
+                // == CAMERA CALIBRATION ==
+                // If you do not manually specify calibration parameters, the SDK will attempt
+                // to load a predefined calibration for your camera.
+                .setLensIntrinsics(578.272, 578.272, 402.145, 221.506)
 
-            // == CAMERA CALIBRATION ==
-            // If you do not manually specify calibration parameters, the SDK will attempt
-            // to load a predefined calibration for your camera.
-            //.setLensIntrinsics(578.272, 578.272, 402.145, 221.506)
+                // ... these parameters are fx, fy, cx, cy.
 
-            // ... these parameters are fx, fy, cx, cy.
-
-            .build();
+                .build();
 
         // Create the vision portal by using a builder.
         VisionPortal.Builder builder = new VisionPortal.Builder();
 
-        // Set the camera (webcam vs. built-in RC phone camera).
-        if (USE_WEBCAM) {
-            builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
-        } else {
-            builder.setCamera(BuiltinCameraDirection.BACK);
-        }
+        // Set the camera
+        builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
 
         // Choose a camera resolution. Not all cameras support all resolutions.
-        //builder.setCameraResolution(new Size(640, 480));
+        builder.setCameraResolution(new Size(640, 360));
 
         // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
-        //builder.enableCameraMonitoring(true);
+        // builder.enableCameraMonitoring(true);
 
         // Set the stream format; MJPEG uses less bandwidth than default YUY2.
         //builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
@@ -144,7 +166,7 @@ public class ConceptAprilTag extends LinearOpMode {
         // Choose whether or not LiveView stops if no processors are enabled.
         // If set "true", monitor shows solid orange screen if no processors enabled.
         // If set "false", monitor shows camera view without annotations.
-        //builder.setAutoStopLiveView(false);
+        builder.setAutoStopLiveView(false);
 
         // Set and enable the processor.
         builder.addProcessor(aprilTag);
@@ -161,29 +183,33 @@ public class ConceptAprilTag extends LinearOpMode {
     /**
      * Add telemetry about AprilTag detections.
      */
-    private void telemetryAprilTag() {
+    private void updateValues() {
 
         List<AprilTagDetection> currentDetections = aprilTag.getDetections();
-        telemetry.addData("# AprilTags Detected", currentDetections.size());
+        double id = 0;
+        double x = 0;
+        double y = 0;
+        double yaw = 0;
+        double bearing = 0;
 
         // Step through the list of detections and display info for each one.
         for (AprilTagDetection detection : currentDetections) {
             if (detection.metadata != null) {
-                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
-                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
-                telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
-                telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
-            } else {
-                telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
-                telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+                //might be able to optimize this by not going through every detection
+                if(detection.id == id) {
+                    x = detection.ftcPose.x;
+                    y = detection.ftcPose.y;
+                    yaw = detection.ftcPose.yaw;
+                    bearing = detection.ftcPose.bearing;
+                    break;
+                }
             }
         }   // end for() loop
+        values[0] = x;
+        values[1] = y;
+        values[2] = yaw;
+        values[3] = bearing;
 
-        // Add "key" information to telemetry
-        telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
-        telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
-        telemetry.addLine("RBE = Range, Bearing & Elevation");
-
-    }   // end method telemetryAprilTag()
+    }   // end method updateValues()
 
 }   // end class
